63ea6c7bfb1e665100a8a5e99c99ce745308c16e
scornik <gutiashik@gmail.com>
2014-12-08 15:53:55 +0000

    pushing rails project files

---
+++ test/controllers/todos_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class TodosControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get add" do
+    get :add
+    assert_response :success
+  end
+
+  test "should get mark" do
+    get :mark
+    assert_response :success
+  end
+
+  test "should get unmark" do
+    get :unmark
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get :delete
+    assert_response :success
+  end
+
+end
